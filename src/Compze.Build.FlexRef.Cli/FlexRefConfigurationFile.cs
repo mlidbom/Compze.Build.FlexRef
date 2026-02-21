bd7839eb1c7276d03e5a22f13ba7486192c82c5d
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-21 11:17:04 +0100

    formatting

--- src/Compze.Build.FlexRef.Cli/FlexRefConfigurationFile.cs
+++ src/Compze.Build.FlexRef.Cli/FlexRefConfigurationFile.cs
@@ -6,6 +6,19 @@ class FlexRefConfigurationFile
 {
     const string ConfigFileName = "FlexRef.config.xml";
 
+    static class Tags
+    {
+        public const string FlexRef = "FlexRef";
+        public const string AutoDiscover = "AutoDiscover";
+        public const string Exclude = "Exclude";
+        public const string Package = "Package";
+    }
+
+    static class Attributes
+    {
+        public const string Name = "Name";
+    }
+
     public DirectoryInfo RootDirectory { get; }
     public string ConfigFilePath { get; }
     public bool UseAutoDiscover { get; private set; }
@@ -24,42 +37,43 @@ class FlexRefConfigurationFile
     {
         var document = XDocument.Load(ConfigFilePath);
         var rootElement = document.Root
-            ?? throw new InvalidOperationException($"Invalid config file: {ConfigFilePath} has no root element.");
+                       ?? throw new InvalidOperationException($"Invalid config file: {ConfigFilePath} has no root element.");
 
-        var autoDiscoverElement = rootElement.Element("AutoDiscover");
+        var autoDiscoverElement = rootElement.Element(Tags.AutoDiscover);
 
         UseAutoDiscover = autoDiscoverElement != null;
 
         AutoDiscoverExclusions = autoDiscoverElement?
-            .Elements("Exclude")
-            .Select(element => element.Attribute("Name")?.Value)
-            .Where(name => name != null)
-            .Select(name => name!)
-            .ToList() ?? [];
+                                .Elements(Tags.Exclude)
+                                .Select(element => element.Attribute(Attributes.Name)?.Value)
+                                .Where(name => name != null)
+                                .Select(name => name!)
+                                .ToList() ?? [];
 
         ExplicitPackageNames = rootElement
-            .Elements("Package")
-            .Select(element => element.Attribute("Name")?.Value)
-            .Where(name => name != null)
-            .Select(name => name!)
-            .ToList();
+                              .Elements(Tags.Package)
+                              .Select(element => element.Attribute(Attributes.Name)?.Value)
+                              .Where(name => name != null)
+                              .Select(name => name!)
+                              .ToList();
     }
 
     public void CreateDefaultConfigFile(List<string> discoveredPackageIds)
     {
         var sortedPackageIds = discoveredPackageIds
-            .OrderBy(id => id, StringComparer.OrdinalIgnoreCase)
-            .ToList();
+                              .OrderBy(id => id, StringComparer.OrdinalIgnoreCase)
+                              .ToList();
 
-        var rootElement = new XElement("FlexRef",
-            new XElement("AutoDiscover"));
+        var rootElement = new XElement(
+            Tags.FlexRef,
+            new XElement(Tags.AutoDiscover));
 
-        if (sortedPackageIds.Count > 0)
+        if(sortedPackageIds.Count > 0)
         {
             //sadly the XComments do not work like real Xml nodes and can only be built using a string, so we have to build the comment text manually here
-            var packageLines = string.Join("\n", sortedPackageIds.Select(id => $"  <Package Name=\"{id}\" />"));
+            var packageLines = string.Join("\n", sortedPackageIds.Select(id => $"  <{Tags.Package} {Attributes.Name}=\"{id}\" />"));
             rootElement.Add(new XComment(
-                $" Alternatively, list packages explicitly instead of using AutoDiscover:\n{packageLines}\n  "));
+                                $" Alternatively, list packages explicitly instead of using {Tags.AutoDiscover}:\n{packageLines}\n  "));
         }
 
         var document = new XDocument(rootElement);
